93e9f0f17a0463fae330b3b46d6909a104b4dfd3
ranaufalmuha <ifal.muha@gmail.com>
2026-04-25 12:07:48 +0700

    fix: keep framer-motion, revert motion package (not compatible)

--- src/shared/components/atoms/ElasticSlider.tsx
+++ src/shared/components/atoms/ElasticSlider.tsx
@@ -4,7 +4,7 @@ import {
     useMotionValue,
     useMotionValueEvent,
     useTransform,
-} from "motion/react";
+} from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import type { ReactNode } from "react";
 
